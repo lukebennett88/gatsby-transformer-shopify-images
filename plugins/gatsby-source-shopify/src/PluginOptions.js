ae3d3e7485af6aa0f8e70d2bbba6eb8ed01ffdf8
mrhut10 <mrhut10@protonmail.com>
2020-05-05 08:55:33 +1000

    created PluginOption singalton class to allow reference to options from anywhere within the plugin

---
+++ plugins/gatsby-source-shopify/src/PluginOptions.js
@@ -0,0 +1,82 @@
+// created this singalton class as a quick way to reference options anywhere within the plugin
+
+class PluginOptions {
+  #shopName;
+
+  #accessToken;
+
+  #apiVersion;
+
+  #verbose;
+
+  #paginationSize;
+
+  #includeCollections;
+
+  #downloadImages;
+
+  get shopName() {
+    return this.#shopName;
+  }
+
+  get accessToken() {
+    return this.#accessToken;
+  }
+
+  get apiVersion() {
+    return this.#apiVersion || '2020-01';
+  }
+
+  get verbose() {
+    return this.#verbose || true;
+  }
+
+  get paginationSize() {
+    return this.#paginationSize || 250;
+  }
+
+  get includeCollections() {
+    return this.#includeCollections || ['shop', 'content'];
+  }
+
+  get downloadImages() {
+    return this.#downloadImages;
+  }
+
+  setShopName(shopName) {
+    this.#shopName = shopName;
+    return this;
+  }
+
+  setAccessToken(accessToken) {
+    this.#accessToken = accessToken;
+    return this;
+  }
+
+  setApiVersion(apiVersion) {
+    this.#apiVersion = apiVersion;
+    return this;
+  }
+
+  setVerbose(verbose) {
+    this.#verbose = verbose;
+    return this;
+  }
+
+  setPaginationSize(paginationSize) {
+    this.#paginationSize = paginationSize;
+    return this;
+  }
+
+  setIncludeCollections(includeCollections) {
+    this.#includeCollections = includeCollections;
+    return this;
+  }
+
+  setDownloadImages(downloadImages) {
+    this.#downloadImages = downloadImages;
+    return this;
+  }
+}
+
+export default new PluginOptions();
